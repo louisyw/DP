10981e092c7020f2f0a477998d62b8512d35fe3a
louisyw <louisyw@126.com>
2016-03-03 10:50:36 +0800

    add 3th solutions to LIS

--- longestIncreaseSubarr/main.cc
+++ longestIncreaseSubarr/main.cc
@@ -7,6 +7,7 @@
 #include <cstdio>
 #include <iostream>
 #include <algorithm>
+#include <string.h>
 using namespace std;
 
 /*递归法*/
@@ -74,10 +75,104 @@ void print_LIS(int array[], int index, int lis, int dp[]){
 		printf("the %d in LIS is %d\n",lis+1, array[index+1]);
 	}
 }
+
+/*sort + LCS*/
+const int MAX_N= 1000;
+int dp[MAX_N][MAX_N];
+
+void qsort(int *array, int left, int right){
+	if (left >= right) return;
+
+	int index = left;
+
+	for(int i = left +1; i <= right; ++i){
+		if (array[i] < array[left])
+			swap(array[i], array[++index]);
+	}
+	swap(array[left], array[index]);
+	qsort(array, left, index-1);
+	qsort(array, index + 1, right);
+}
+
+int LCS(int *arr, int len1, int *sort_arr, int len2){
+	/*全局变量，没有必要初始化*/
+	for (int i = 0; i < len1; ++i){
+		dp[i][0]  = 0;
+	}
+	for (int i = 0 ; i < len2; ++i){
+		dp[0][i] = 0;
+	}
+
+	for(int i = 1; i <= len1; ++i){
+		for(int j = 1; j <= len2; ++j){
+			if (arr[i - 1 ] == sort_arr[j - 1]){
+				dp[i][j] = dp[i-1][j-1] + 1;
+			}
+			else{
+				dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+			}
+		}
+	}
+
+	return dp[len1][len2];
+}
+
+
+/*end[i]记录长度为i的递增自序列的最小元素*/
+
+/*arr为有序序列，判断在arr中大于等于val的最小下标*/
+int upper_bound(int *arr, int length, int val){
+
+	int left = 1, right = length;
+
+	if (arr[right] < val)
+		return right+1;
+
+	//while(left < right)   //此处注意循环终止的条件
+	while(right - left > 1){
+		
+		int mid = (left + right)/2;
+		if (arr[mid] >= val)
+			right = mid;
+		else 
+			left = mid;
+	}
+	return right;
+}
+
+int LIS2(int *arr, int length){
+	
+	int end[length + 1];   //end[0]不用
+
+	end[1] = arr[0];
+
+	int len = 1;
+	for(int i = 1; i < length; i++){
+		int index = upper_bound(end, len, arr[i]);
+		end[index] = arr[i];
+
+		if(len < index)
+			len = index;
+	}
+	return len;
+}
+
+
 int main(){
 	int a[] = { 10, 22, 9, 33, 21, 50, 41, 60 };
 	int n = sizeof(a)/sizeof(int);
+
+	printf("using the last record:\n");
+	printf("the longest subarray length is %d\n", LIS2(a, n));
+	int sort[n];
+	memcpy(sort, a ,sizeof(a));
+	qsort(sort, 0, n - 1);
+
+	printf("using sort and LCS:\n");
+	printf("the longest subarray length is %d\n",LCS(a, n, sort, n));
+	printf("using recursice:\n");
 	printf("the longest subarray length is %d\n",LIS(a, n));
+	printf("using DP:\n");
 	printf("the longest subarray length is %d\n",LIS_DP(a, n));
 	return 0;
 }
